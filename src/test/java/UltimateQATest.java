14b4ff7af0bd44bf561df522158ee8b0ce146e49
Tomasz Oskroba <toskroba@sii.pl>
2023-12-17 17:26:30 +0100

    Final changes

--- src/test/java/UltimateQATest.java
+++ src/test/java/UltimateQATest.java
@@ -31,6 +31,14 @@ public class UltimateQATest {
 
         // assertions
         WebElement text = driver.findElementByClassName("entry-title");
+        //     //*[@class='...'] - ogolna postac xpath
+        //     //*[@class='...' and @id='...']//*[@id='']
+        //     //*[contains(@class, '...')]
+        //     //*[contains(@class, 'et_pb_module et_pb_cta_0 et_pb_promo')]
+        //     //div[@class='navbar-header']//img[@alt='Nasa']
+        //     //*[text()='MORE STORIES']
+        //     / - jeden poziom nizej
+        //     // - dwa lub wiecej poziomow nizej
         //  WebElement textUsingXpath = driver.findElementByXPath("//*[@class='entry-title']");
         Assertions.assertEquals("Button success", text.getText());
     }
@@ -39,7 +47,7 @@ public class UltimateQATest {
     @AfterEach
     public void teardown() {
         // roznica driver.quit() a driver.close()
-//        driver.quit();
+        driver.quit();
     }
 
     @Test
@@ -52,13 +60,13 @@ public class UltimateQATest {
 
         // czas oczekiwan w Selenium
         // implicit wait / explicit wait / fluent wait / Thread.sleep
-
-        //assercję
     }
 
     @Test
     public void testThree() {
-
+        WebElement blueField = driver.findElementByXPath("//*[contains(@class, 'et_pb_module et_pb_cta_0 et_pb_promo')]");
+        // zmiana koloru HEX na RGBA
+        Assertions.assertEquals("rgba(46, 163, 242, 1)", blueField.getCssValue("background-color"));
     }
 
 }
